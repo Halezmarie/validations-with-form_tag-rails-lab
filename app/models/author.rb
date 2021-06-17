6237d1c99ec9d85b6165266e3455ba898a8d0bce
halezmarie <teacherhaleymarie@gmail.com>
2021-06-17 09:21:05 -0500

    commit tree 21ddef90ce6d38b9486d50618bdf6bbe77d92804

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
-  validates :phone_number, length: { minimum: 10, maximum: 10 }
+  validates :phone_number, length: { minimum: 10, maximum: 10 } # EXACTLY 10 digits long
+  
 end
